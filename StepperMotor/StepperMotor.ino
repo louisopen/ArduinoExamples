31d97136f7d81630c23749347e21a25f5707eafa
Louis Huang <louisopen@gmail.com>
2019-05-19 23:07:45 +0800

    update stepper

--- StepperMotor/StepperMotor.ino
+++ StepperMotor/StepperMotor.ino
@@ -1,18 +1,15 @@
 #include <Stepper.h>
- 
+
 const int stepsPerRevolution = 2048;  
-// change this to fit the number of steps per revolution
-// for your motor
+// change this to fit the number of steps per revolution for your motor
  
 // initialize the stepper library on pins 8 through 11:
 //Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
  
 void setup() {
-  // set the speed at 60 rpm:
-  myStepper.setSpeed(5);
-  // initialize the serial port:
-  Serial.begin(9600);
+  myStepper.setSpeed(5);  //set the speed at 60 rpm: 
+  Serial.begin(9600);   //initialize the serial port:
 }
  
 void loop() {
@@ -26,3 +23,16 @@ void loop() {
   myStepper.step(-stepsPerRevolution);
   delay(500);
 }
+
+/*
+void setup() {
+  // nothing to do
+}
+void loop() {
+  myStepper.setSpeed(1); // 1 rpm
+  myStepper.step(2038); // do 2038 steps -- corresponds to one revolution in one minute
+  delay(1000); // wait for one second
+  myStepper.setSpeed(6); // 6 rpm
+  myStepper.step(-2038); // do 2038 steps in the other direction with faster speed -- corresponds to one revolution in 10 seconds
+}
+*/
