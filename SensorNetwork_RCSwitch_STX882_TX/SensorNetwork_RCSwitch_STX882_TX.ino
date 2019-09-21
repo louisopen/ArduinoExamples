45937cde52ab347957aa2167ced15ef77585fc26
Louis Huang <louisopen@gmail.com>
2019-09-21 22:07:06 +0800

    update

--- SensorNetwork_RCSwitch_STX882_TX/SensorNetwork_RCSwitch_STX882_TX.ino
+++ SensorNetwork_RCSwitch_STX882_TX/SensorNetwork_RCSwitch_STX882_TX.ino
@@ -1,22 +1,30 @@
-//*-- Sensor data by 433MHz --*//
-#define SENSOR_ID0 0x33300000 //0xFFFE0000 address(15bits) + 0x0000FFFF data(16bits) for Ain6,Ain7
-#define SENSOR_ID1 0x33320000 //0xFFFE0000 address(15bits) + 0x00010000 sign bit(1bit) + 0x0000FFFF data(16bits) for Temp & Hum
-#define SENSOR_ID2 0x33340000 //0xFFFE0000 address(15bits) + 0x0000FFFF data(16bits)
-#define SENSOR_ID3 0x333330   //0xFFFFF0   address(20bits) + 0xF data(4bits) for keyfob
+//*-- Sensor applications (DS18b20,NTC103AT,DHT22)to RF 433MHz --*//
+#define SENSOR_TYPE_DS18b20   //魚池溫度用,五米長, 若無定義為DHT22 sensor  Louis:20190911
+
+#include <avr/wdt.h>          //For System Watching-Dog
+#include <avr/sleep.h>        //For System Sleep mode operation
+#include <avr/power.h>        //For System power setting operation
+
+//*-- Define RF ID data by 433MHz --*//
+#define SENSOR_ID0 0x33300000 //0xFFFE0000 address(15bits) + 0x0000FFFF data(16bits) for Ain6,Ain7    //ADC數據專用
+#define SENSOR_ID1 0x33320000 //0xFFFE0000 address(15bits) + 0x00010000 sign bit(1bit)+0x0000FFFF data(16bits) //DHT22溫濕度數據專用
+#define SENSOR_ID2 0x33340000 //0xFFFE0000 address(15bits) + 0x00010000 sign bit(1bit)+0x0000FFFF data(16bits) //DS18b20溫度數據專用
+#define SENSOR_ID3 0x33333000 //0xFFFFF0   address(20bits) + 0xF data(4bits) for keyfob
 //#define DataBit 24            //24 bits shift out standar application for Key-chan or keyfob
-#define DataBit 32            //32 bits shift out
-#define OnBoardLED 13         //D13 for transmit
+#define DataBit 32            //32 bits shift out 24~32 bits
+#define OnBoardLED 13         //D13 for the system states
 
 //*-- ASK433 module --*//
 #include <RCSwitch.h>
 RCSwitch myRFTX = RCSwitch();    
-#define RF_tx 2               //D2 for RF serial 
+#define RF_tx 4               //D2 D3 D4 for RF serial transmit
+#define RF_rx 1               //int0(D2) int1(D3) for RF serial received
 //RCSwitch mySwitch = RCSwitch(); 
 
 //*-- DHT22 --*//
 #include <Wire.h>OnBoardLED
 #include "DHT.h"
-#define _dhtpin   A3          //D4
+#define _dhtpin   A3          //A3
 #define _dhttype  DHT22       //DHT 22 (AM2302), AM2321
 //#define _dhttype DHT21      //DHT 21 (AM2301)
 DHT dht22( _dhtpin, _dhttype );
@@ -24,9 +32,16 @@ DHT dht22( _dhtpin, _dhttype );
 #define _ain7     7           //A7
 #define _ain6     6           //A6
 
-#include <avr/wdt.h>          //For Watching Dog
-#include <avr/sleep.h>        //For Sleep operation
-#include <avr/power.h>        //For Sleep operation
+#include <OneWire.h>
+#include <DallasTemperature.h>
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS 2        //D2
+// Setup a oneWire instance to communicate with any OneWire devices 
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
 
 unsigned long buf_first=0;    //long 類型有32bit，24/32才夠用或是位元旋轉才夠。
 //unsigned long buf_second=0;   //long 類型有32bit，24/32才夠用或是位元旋轉才夠。
@@ -65,20 +80,22 @@ void setup_timer(){ //timer for wakeup from sleep mode
 }
 /**********************************************************/
 void init_application(){
-  myRFTX.enableTransmit(2);    //发送端接D2号口（或其它口）  
-  myRFTX.setProtocol(2);       //非Remote Control (default is 1, will work for most outlets)
-  //myRFTX.setPulseLength(320); //Optional set pulse length(uS) 
-  myRFTX.setRepeatTransmit(6); //Optional set number of transmission repetitions
-  //mySwitch.enableReceive(0); //接收端接中斷INT0(D2),中斷INT1(D3)
+  myRFTX.enableTransmit(RF_tx);     //发送端接D2号口（或其它口）  
+  myRFTX.setProtocol(2);            //非Remote Control (default is 1, will work for most outlets)
+  //myRFTX.setPulseLength(320);     //Optional set pulse length(uS) 
+  myRFTX.setRepeatTransmit(6);      //Optional set number of transmission repetitions
+  //mySwitch.enableReceive(RF_rx);  //接收端接中斷INT0(D2),中斷INT1(D3)
 
   dht22.begin();
+  
+  sensors.begin();
 }
 /**********************************************************/
 /**********************************************************/
 /**********************************************************/
 /**********************************************************/
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(115200); //for debug
   pinMode(OnBoardLED, OUTPUT );
   Serial.println("Initial successful ID=0x3333(12bits):function+Sign(4bits):data(16bits)");
 
@@ -89,10 +106,39 @@ void setup(){
 /**********************************************************/
 /**********************************************************/
 void loop(){
-  if(flag_timer==1)
+  if(flag_timer==1) //wakeup around for 4seconds 
   {
     flag_timer = 0;
-    // Reading temperature or humidity takes about 250 milliseconds!
+#ifdef  SENSOR_TYPE_DS18b20
+    //Reading temperature sensor from DS18b20
+    sensors.requestTemperatures(); // Send the command to get temperatures  
+    float t = sensors.getTempCByIndex(0);
+    Serial.print("Temperature is: ");
+    Serial.println(t); // Why "byIndex"?  0 refers to the first IC on the wire
+    buf_first = SENSOR_ID2; //DS18b20 溫濕度數據專用
+    unsigned int x=0;  //取整數用
+    if(t<0)
+    {      
+      x= 0.0 - t*10; //翻正
+      //Serial.print( x );
+      //Serial.print("  ");
+      //buf_first |= (x%512) << 7;    //取最高9位
+      buf_first |= x;
+      buf_first |= 0x00010000;   //mask & clear "-" sign flag "負"
+    }
+    else
+    {
+      x=t*10; 
+      //Serial.print( x );
+      //Serial.print("  ");
+      //buf_first |= (x%512) << 7;    //取最高9位
+      buf_first |= x;
+    }      
+    //buf_first &= 0xFFFFFF80;
+    //buf_first |= (uint8_t)h & 0x7F; //取最高7位   
+    Serial.println(buf_first,HEX);
+#else
+    //Reading temperature sensor from DHT22 module
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
     float h = dht22.readHumidity();
     // Read temperature as Celsius (the default)
@@ -116,16 +162,15 @@ void loop(){
     else
     {
       Serial.print( "ReadDHT:" );
-      buf_first = SENSOR_ID1;
-      //DHT22 溫濕度數據儲存
-      uint8_t x=0; 
+      buf_first = SENSOR_ID1;      //DHT22 溫濕度數據專用
+      unsigned int x=0; //取整數用
       if(t<0)
       {      
-        x= 0.0 - t*10; 
+        x= 0.0 - t*10; //翻正
         //Serial.print( x );
         //Serial.print("  ");
         buf_first |= (x%512) << 7;    //取最高9位
-        buf_first |= 0x00010000;   //mask & clear "-" sign flag
+        buf_first |= 0x00010000;   //mask & clear "-" sign flag "負"
       }
       else
       {
@@ -138,7 +183,7 @@ void loop(){
       buf_first |= (uint8_t)h & 0x7F; //取最高7位   
     }
     Serial.println(buf_first,HEX);
-  
+#endif  
     //buf = buf+1;
     digitalWrite(OnBoardLED,HIGH);    //RF data 發送期間亮起
     myRFTX.send(buf_first, DataBit);  //24/32bits Transmit & received test OK, 
